9b90b590c2b867c7895f6066793001bc73cd5f19
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2021-01-08 17:16:08 +0100

    Update __init__.lua

--- ext/shared/__init__.lua
+++ ext/shared/__init__.lua
@@ -6,19 +6,19 @@ Events:Subscribe('Level:LoadingInfo', function(screenInfo)
 
         -- Bazaar
         if SharedUtils:GetLevelName():match("MP_001") then
-              if Map.Bazaar = ('true, night') then
+              if Map.Bazaar == ('true, night') then
                   Preset.Night
                   print('Using Preset "Night" on Grand Bazaar')
-              elseif Map.Bazaar = ('true, morning') then
+              elseif Map.Bazaar == ('true, morning') then
                   Preset.Morning
                   print('Using Preset "Morning" on Grand Bazaar')
-              elseif Map.Bazaar = ('true, noon') then
+              elseif Map.Bazaar == ('true, noon') then
                   Preset.Noon
                   print('Using Preset "Noon" on Grand Bazaar')
-              elseif Map.Bazaar = ('true, evening') then
+              elseif Map.Bazaar == ('true, evening') then
                   Preset.Evening
                   print('Using Preset "Evening" on Grand Bazaar')
-              elseif Map.Bazaar = (false, none) then
+              elseif Map.Bazaar == (false, none) then
                   print('Not Changing Map Time')
                   return end
               else
