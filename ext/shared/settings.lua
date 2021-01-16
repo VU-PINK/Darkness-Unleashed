2d9c36f0a9a36446e089dd30f6787ea53d46f7d2
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2021-01-16 21:53:08 +0100

    Update settings.lua

--- ext/shared/settings.lua
+++ ext/shared/settings.lua
@@ -19,45 +19,45 @@
 
 
         --Standard
-        ['Bazaar.Night']                      = true,             --MP_001      | Grand Bazaar
-        ['Teheran.Morning']                   = true,             --MP_003      | Teheran Highway
-        ['Caspian.Morning']                   = true,             --MP_007      | Caspian Border
-        ['Seine.Night']                       = true,             --MP_011      | Seine Crossing
-        ['Firestorm.Morning']                 = true,             --MP_012      | Operation Firestorm
-        ['Davamand.Night']                    = true,             --MP_013      | Davamand Peak
-        ['Noshahr.Night']                     = true,             --MP_017      | Noshahr Canals
-        ['Kharg.Night']                       = true,             --MP_018      | Kharg Island
-        ['Metro.Night']                       = true,             --MP_Subway   | Operation Metro
+        ['Bazaar.Night']                        = true,             --MP_001      | Grand Bazaar
+        ['Teheran.Night']                       = true,             --MP_003      | Teheran Highway
+        ['Caspian.Night']                       = true,             --MP_007      | Caspian Border
+        ['Seine.Night']                         = true,             --MP_011      | Seine Crossing
+        ['Firestorm.Night']                     = true,             --MP_012      | Operation Firestorm
+        ['Davamand.Night']                      = true,             --MP_013      | Davamand Peak
+        ['Noshahr.Night']                       = true,             --MP_017      | Noshahr Canals
+        ['Kharg.Night']                         = true,             --MP_018      | Kharg Island
+        ['Metro.Night']                         = true,             --MP_Subway   | Operation Metro
 
         --DLC Back to Karkand
-        ['Karkand.Evening']                   = true,             --XP1_001     | Strike at Karkand
-        ['Gulf.Bright_Night']                 = true,             --XP1_002     | Gulf of Oman
-        ['Sharqi.Night']                      = true,             --XP1_003     | Sharqi Peninsula
-        ['Wake.Morning']                      = true,             --XP1_004     | Wake Island
+        ['Karkand.ENight']                      = true,             --XP1_001     | Strike at Karkand
+        ['Gulf.Night']                          = true,             --XP1_002     | Gulf of Oman
+        ['Sharqi.Night']                        = true,             --XP1_003     | Sharqi Peninsula
+        ['Wake.Night']                          = true,             --XP1_004     | Wake Island
 
         --DLC Close Quarters
-        ['Donya.Night']                       = true,             --XP2_Palace  | Donya Fortress
-        ['Operation925.Night']                = true,             --XP2_Office  | Operation 925
-        ['Scrapmetal.Night']                  = true,             --XP2_Factory | Scrapmetal
-        ['Ziba.Night']                        = true,             --XP2_Skybar  | Ziba Tower
+        ['Donya.Night']                         = true,             --XP2_Palace  | Donya Fortress
+        ['Operation925.Night']                  = true,             --XP2_Office  | Operation 925
+        ['Scrapmetal.Night']                    = true,             --XP2_Factory | Scrapmetal
+        ['Ziba.Night']                          = true,             --XP2_Skybar  | Ziba Tower
 
         --DLC Armored Kill
-        ['Alborz.Night']                      = true,             --XP3_Alborz  | Alborz Mountains
-        ['Shield.Bright_Night']               = true,             --XP3_Shield  | Armored Shield
-        ['Bandar.Morning']                    = true,             --XP3_Desert  | Bandar Desert
-        ['Death.Morning']                     = true,             --XP3_Valley  | Death Valley
+        ['Alborz.Night']                        = true,             --XP3_Alborz  | Alborz Mountains
+        ['Shield.Night']                        = true,             --XP3_Shield  | Armored Shield
+        ['Bandar.Night']                        = true,             --XP3_Desert  | Bandar Desert
+        ['Death.Night']                         = true,             --XP3_Valley  | Death Valley
 
         --DLC Aftermath
-        ['Azadi.Night']                       = true,             --XP4_Parl    | Azadi Palace
-        ['Epicenter.Night']                   = true,             --XP4_Quake   | Epicenter
-        ['Markaz.Night']                      = true,             --XP4_FD      | Markaz Monolith
-        ['Talah.Night']                       = true,             --XP4_Rubble  | Talah Market
+        ['Azadi.Night']                         = true,             --XP4_Parl    | Azadi Palace
+        ['Epicenter.Night']                     = true,             --XP4_Quake   | Epicenter
+        ['Markaz.Night']                        = true,             --XP4_FD      | Markaz Monolith
+        ['Talah.Night']                         = true,             --XP4_Rubble  | Talah Market
 
         --DLC End Game
-        ['Riverside.Night']                   = true,             --XP5_001     | Operation Riverside
-        ['Nebandan.Morning']                  = true,             --XP5_002     | Nebandan Flats
-        ['Kiasar.Night']                      = true,             --XP5_003     | Kiasar Railroad
-        ['Pipeline.Morning']                  = true,             --XP5_004     | Sabalan Pipeline
+        ['Riverside.Night']                     = true,             --XP5_001     | Operation Riverside
+        ['Nebandan.Night']                      = true,             --XP5_002     | Nebandan Flats
+        ['Kiasar.Night']                        = true,             --XP5_003     | Kiasar Railroad
+        ['Pipeline.Night']                      = true,             --XP5_004     | Sabalan Pipeline
 
         }
 
