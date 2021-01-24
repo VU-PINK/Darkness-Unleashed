cfa230a66d0c5b54e1d930f4e788c9e0425a52dc
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2021-01-24 18:10:24 +0100

    new system

--- ext/client/night.lua
+++ ext/client/night.lua
@@ -10,24 +10,28 @@ local sunFlare = nil
 
 
 function Night(Map)
-	--Code Idea/Concept credit to https://github.com/OrfeasZ
+	if night ~= nil then
+		return
+	end
 
-	night = VisualEnvironmentState()
-	night.visibility = 1.0
-	night.priority = 100
+	local nightData = VisualEnvironmentEntityData()
+	nightData.enabled = true
+	nightData.visibility = 1.0
+	nightData.priority = 999999
 
-	--shaderParams = ShaderParamsData()
-	--shaderParams.value = Vec4(1.0, 1.0, 1.0, 1.0)
-	--shaderParams.parameterName = 'FLIRData'
+	local shaderParams = ShaderParamsComponentData()
+	shaderParams.value = Vec4(1.0, 1.0, 1.0, 1.0)
+	shaderParams.parameterName = 'FLIRData'
 
-	outdoorLight = OutdoorLightData()
+	local outdoorLight = OutdoorLightComponentData()
 	outdoorLight.enable = true
 	outdoorLight.sunColor = Vec3(0.02, 0.02, 0.02)
-	outdoorLight.skyColor = Vec3(0.01, 0.01, 0.01)
-	outdoorLight.groundColor = Vec3(0, 0, 0)
+	outdoorLight.skyColor = Vec3(0.01, 0.01, 0.025)
+	outdoorLight.groundColor = Vec3(0.01, 0.01, 0.01)
+	outdoorLight.skyEnvmapShadowScale = 0.25
 
-	sky = SkyComponentData()
-	sky.brightnessScale = 0.0005
+	local sky = SkyComponentData()
+	sky.brightnessScale = 0.00005--*BrightnessMultiplicator
 	sky.sunSize = 0
 	sky.sunScale = 0
 	sky.cloudLayer1SunLightIntensity = 0.0001
@@ -39,62 +43,79 @@ function Night(Map)
 	sky.staticEnvmapScale = 0.1
 	sky.skyEnvmap8BitTexScale = 1
 	sky.customEnvmapAmbient = 0.05
-	sky.panoramicRotation = 100
 
-	fog = FogData()
-	fog.enable = true
-	fog.fogDistanceMultiplier = 1.0
-	fog.fogGradientEnable = true
-	fog.start = 2
-	fog.endValue = 65
-	fog.curve = Vec4(3.108949, -4.2201934, 2.0970724, -0.001664313)
-	fog.fogColorEnable = true
-	fog.fogColor = Vec3(0.0005, 0.0005, 0.0000000015)
-	fog.fogColorStart = 2
-	fog.fogColorEnd = 40
-	fog.fogColorCurve = Vec4(0.30, 0.20, 0.15, 0)
-	fog.transparencyFadeStart = -50
-	fog.transparencyFadeEnd = 50
-	fog.transparencyFadeClamp = 1.0
+	local colorCorrection = ColorCorrectionComponentData()
+	colorCorrection.enable = true
+	colorCorrection.brightness = Vec3(1, 1, 1)
+	colorCorrection.contrast = Vec3(1.05, 1.05, 1.05)
+	colorCorrection.saturation = Vec3(1.0, 1.025, 1.15)
 
-	tonemap = TonemapData()
+	local tonemap = TonemapComponentData()
 	tonemap.minExposure = 0.2
 	tonemap.maxExposure = 4
 	tonemap.exposureAdjustTime = 1.5
 	tonemap.bloomScale = Vec3(0.05, 0.05, 0.05)
-	tonemap.tonemapMethod = 3
+	tonemap.tonemapMethod = 2
 
-	cc = ColorCorrectionData()
-	cc.enable = true
-	cc.contrast = Vec3(1.05, 1.05, 1.05)
-	cc.saturation = Vec3(1.22, 1.25, 1.5)
+	local fog = FogComponentData()
+	fog.enable = true
+	--fog.fogDistanceMultiplier = 1.0
+	--fog.fogGradientEnable = true
+	fog.start = 1
+	fog.endValue = 25
+	fog.curve = Vec4(0.45, 0.35, 0.25, -2)
+	fog.fogColorEnable = true
+	fog.fogColor = Vec3(0.0001, 0.0001, 0.0001)
+	fog.fogColorStart = 2
+	fog.fogColorEnd = 30
+	fog.fogColorCurve = Vec4(0.4, 0.35, 0.2, -2)
+	fog.transparencyFadeEnd = 50
+	fog.transparencyFadeClamp = 1.0
 
-	enlighten = EnlightenData()
+	local enlighten = EnlightenComponentData()
 	enlighten.enable = false
 
-	sunFlare = SunFlareData()
+	local sunFlare = SunFlareComponentData()
 	sunFlare.enable = false
 
-	-- Add components to VE state.
-	night.outdoorLight = outdoorLight
-	night.vignette = vignette
-	night.fog = fog
-	night.colorCorrection = cc
-	night.enlighten = enlighten
-	night.sunFlare = sunFlare
-	night.tonemap = tonemap
+	--nightData.components:add(shaderParams)
+	--nightData.runtimeComponentCount = nightData.runtimeComponentCount + 1
 
-	--night:AddShaderParams(shaderParams)
-	-- Add state to VE manager.
-	VisualEnvironmentManager:AddState(night)
+	nightData.components:add(outdoorLight)
+	nightData.runtimeComponentCount = nightData.runtimeComponentCount + 1
+
+	nightData.components:add(colorCorrection)
+	nightData.runtimeComponentCount = nightData.runtimeComponentCount + 1
+
+	nightData.components:add(tonemap)
+	nightData.runtimeComponentCount = nightData.runtimeComponentCount + 1
+
+	nightData.components:add(fog)
+	nightData.runtimeComponentCount = nightData.runtimeComponentCount + 1
+
+	nightData.components:add(sky)
+	nightData.runtimeComponentCount = nightData.runtimeComponentCount + 1
+
+	nightData.components:add(enlighten)
+	nightData.runtimeComponentCount = nightData.runtimeComponentCount + 1
+
+	nightData.components:add(sunFlare)
+	nightData.runtimeComponentCount = nightData.runtimeComponentCount + 1
+
+
+	nightPreset = EntityManager:CreateEntity(nightData, LinearTransform())
+
+	if nightPreset ~= nil then
+		nightPreset:Init(Realm.Realm_Client, true)
+	end
 
 end
 
 function removeNight()
 
-	if night ~= nil then
-		night:Destroy()
-		night = nil
+	if nightPreset ~= nil then
+		nightPreset:Destroy()
+		nightPreset = nil
 	end
 
 	print('removed VES')
