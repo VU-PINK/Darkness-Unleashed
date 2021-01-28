9bbdadec8a94ebfbbb92d6f8d5fc6c35c75921f5
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2021-01-28 19:14:14 +0100

    live-wip

--- ext/client/interchangable.lua
+++ ext/client/interchangable.lua
@@ -14,7 +14,7 @@
 
 --- Skybox Functions
 
-function MoonNightSky()
+
     --- Load Bundles | Death Valley Skybox
     -- Mount
     Events:Subscribe('Level:LoadResources', function()
@@ -70,57 +70,4 @@ function MoonNightSky()
 
     	    end
     end)
-end
-
-
-function EveningSky()
-      --- Load Bundles | Bandar Desert Skybox
-      -- Mount
-      Events:Subscribe('Level:LoadResources', function()
-
-      	    ResourceManager:MountSuperBundle('levels/xp3_desert/xp3_desert') -- Death Valley Bright Night Skybox & Star Clouds
-
-      	end)
-
-      -- Inject
-      Hooks:Install('ResourceManager:LoadBundles', 100, function(hook, bundles, compartment)
-      	    if #bundles == 1 and bundles[1] == SharedUtils:GetLevelName() then
-      	        print('Injecting bundles.')
-
-      	       bundles = {
-      	    'levels/xp3_desert/xp3_desert',  -- Death Valley Sky
-      	        bundles[1],
-      	       }
-
-      	        hook:Pass(bundles, compartment)
-
-
-      --- Register
-            local partitionID5 = Guid('2C79B362-92A2-43D7-A929-4682B10E42B4')                         -- Panoramic Texture
-            local partitionID6 = Guid('9C228323-EED2-428F-B2E6-051A3119ADFC')                         -- Panoramic Alpha Texture
-            local partitionID7 = Guid('AC4422AB-431C-4417-A3EE-5BD1B1DD6D00')                         -- Sky Gradient
-            local partitionID8 = Guid('D7766140-9B4C-11E1-A175-EFA690A2F651')                         -- Static Envmap Texture
-            local PanoramicinstanceID5 = Guid('D09201F5-B89A-4121-972F-1742A73D8F60')                 -- [1]Instance
-            local AlphainstanceID6 = Guid('E870E98D-B798-4EB8-B930-49BF477F45A6')                     -- [2]Instance
-            local SkyGradientID7 = Guid('C7662BD6-BB47-4C03-9939-DA2EC7EE43E5')                       -- [3]Instance
-            local sEnvMapID8 = Guid('22AA9B86-7973-07FF-D7DD-9A69970E7B14')                           -- [4]Instance
-
-                ResourceManager:RegisterInstanceLoadHandler(partitionID5, PanoramicinstanceID5, function(panoramicTextureInstance)        -- Register Instance
-                EveningSkybox = panoramicTextureInstance                                                                                  -- Make Global For Easy Access
-                end)
-
-                ResourceManager:RegisterInstanceLoadHandler(partitionID6, AlphainstanceID6, function(panoramicAlphaTextureInstance)       -- Register Instance
-                EveningAlpha = panoramicAlphaTextureInstance                                                                              -- Make Global For Easy Access
-                end)
-
-                ResourceManager:RegisterInstanceLoadHandler(partitionID7, SkyGradientID7, function(SkyGradientInstance)                   -- Register Instance
-                EveningGradient = SkyGradientInstance                                                                                     -- Make Global For Easy Access
-                end)
-
-                ResourceManager:RegisterInstanceLoadHandler(partitionID8, sEnvMapID8, function(sEnvmapTextureInstance)                    -- Register Instance
-                EveningEnvmap = sEnvmapTextureInstance                                                                                    -- Make Global For Easy Access
-                end)
-
-        	  end
-      end)
 end
