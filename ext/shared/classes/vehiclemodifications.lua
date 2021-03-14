fd783dce07aa4dc2b8aa7f90c9c4d73518f0d745
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2021-03-14 23:31:46 +0100

    Update vehiclemodifications.lua

--- ext/shared/classes/vehiclemodifications.lua
+++ ext/shared/classes/vehiclemodifications.lua
@@ -1,15 +1,15 @@
 Events:Subscribe('Level:RegisterEntityResources', function(levelData)
 
-	-- HMMVWV 
+	-- HMMVWV
 	local hmveeChassisComponentData = nil
-	if ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("611F48A3-0919-11E0-985D-C512734E48AF"), Guid("DA89EB6C-A824-A813-A6C5-E2DEF9D4F4FF"))) then 
-		hmmwvChassisComponentData = ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("611F48A3-0919-11E0-985D-C512734E48AF"), Guid("DA89EB6C-A824-A813-A6C5-E2DEF9D4F4FF")))
-	elseif ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("F7C250D2-ECEB-481F-A130-D91FE8B693E0"), Guid("5D10A701-196F-4DA9-8D83-610ABF56124B"))) then
+	--if ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("611F48A3-0919-11E0-985D-C512734E48AF"), Guid("DA89EB6C-A824-A813-A6C5-E2DEF9D4F4FF"))) then
+		--hmmwvChassisComponentData = ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("611F48A3-0919-11E0-985D-C512734E48AF"), Guid("DA89EB6C-A824-A813-A6C5-E2DEF9D4F4FF")))
+	--elseif ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("F7C250D2-ECEB-481F-A130-D91FE8B693E0"), Guid("5D10A701-196F-4DA9-8D83-610ABF56124B"))) then
 		hmmwvChassisComponentData = ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("F7C250D2-ECEB-481F-A130-D91FE8B693E0"), Guid("5D10A701-196F-4DA9-8D83-610ABF56124B")))
-	else 
-		print("No HMMVWV found")
-		return
-	end
+	--else
+	--	print("No HMMVWV found")
+	--	return
+	--end
 
 	if hmmwvChassisComponentData ~= nil then
 		-- Spotlights
@@ -73,26 +73,53 @@ Events:Subscribe('Level:RegisterEntityResources', function(levelData)
 		vehicleLightComponentDataFar2.light = vehicleSpotLightFar
 
 		vehicleLightComponentDataFar2.transform = LinearTransform(
-			Vec3(-1, 0, 0), --rotation  
-			Vec3(0, 1, 0), 
-			Vec3(0, 0, 1), 
+			Vec3(-1, 0, 0), --rotation
+			Vec3(0, 1, 0),
+			Vec3(0, 0, 1),
 			Vec3(0.525, 0.65, 3) --pos (sideways,height,frontback)
 		)
 
 		-- PointLights
+		local VehiclePointlight = PointLightEntityData()
+		VehiclePointlight.width = 0.05
 
+		local VehiclePointlightComponentData = LightComponentData()
+		VehiclePointlightComponentData.light = VehiclePointlight
+
+		VehiclePointlightComponentData.transform = LinearTransform(
+			Vec3(-1, 0, 0), --rotation
+			Vec3(0, 1, 0),
+			Vec3(0, 0, 1),
+			Vec3(0, 1, 5) --pos (sideways,height,frontback)
+		)
 
 		-- Lens Flares
 		local LensFlare = LensFlareEntityData(ResourceManager:FindInstanceByGuid(Guid("65A5BFD9-028A-4D4F-8B89-3A60B2E06F83"), Guid("D8DB98E1-AEBA-485E-9AA4-D5F55C5CDECE")))
 		local LensFlare1 = LensFlareComponentData()
 		LensFlare1.lensFlare = LensFlare
 
+		LensFlare:MakeWritable()
+
+		for key, value in pairs(LensFlare.elements) do
+			value.sizeCamDistMax = value.sizeCamDistMax*2
+			value.sizeAngleCurve = value.sizeAngleCurve*1
+			value.size = value.size*0.34
+			value.sizeOccluderCurve = value.sizeOccluderCurve*1.5
+			value.sizeScreenPosCurve = value.sizeScreenPosCurve*1.5
+			value.alphaCamDistCurve = value.alphaCamDistCurve*1.5
+			value.alphaOccluderCurve = value.alphaOccluderCurve*1.5
+			value.alphaAngleCurve = value.alphaScreenPosCurve*1
+			value.alphaScreenPosCurve = value.alphaScreenPosCurve*1.5
+			value.alphaCamDistMax = value.alphaCamDistMax*2
+			print("LensFlare Changed")
+		end
+
 		LensFlare1.transform = LinearTransform(
 			Vec3(-1.0, 0.0, 8.74227765735e-08), --rotation
 			Vec3(0, 1, 0),
 			Vec3(-8.74227765735e-08, 0, -1),
-			Vec3(-0.525, 1.5, 3) --pos (sideways,height,frontback)
-		)
+			Vec3(0.55, 1, 2.7) --pos (sideways,height,frontback)
+			)
 
 		local LensFlare2 = LensFlareComponentData()
 		LensFlare2.lensFlare = LensFlare
@@ -101,8 +128,8 @@ Events:Subscribe('Level:RegisterEntityResources', function(levelData)
 			Vec3(-1.0, 0.0, 8.74227765735e-08), --rotation
 			Vec3(0, 1, 0),
 			Vec3(-8.74227765735e-08, 0, -1),
-			Vec3(0, 0, 0) --pos (sideways,height,frontback)
-		)
+			Vec3(-0.55, 1, 2.7) --pos (sideways,height,frontback)
+			)
 
 		-- Add new components to Vehicle Chassis
 
@@ -112,38 +139,257 @@ Events:Subscribe('Level:RegisterEntityResources', function(levelData)
 		hmmwvChassisComponentData.components:add(vehicleLightComponentData2)
 		hmmwvChassisComponentData.components:add(vehicleLightComponentDataFar)
 		hmmwvChassisComponentData.components:add(vehicleLightComponentDataFar2)
-		--hmmwvChassisComponentData.components:add(LensFlare1)
-		--hmmwvChassisComponentData.components:add(LensFlare2)
+		hmmwvChassisComponentData.components:add(VehiclePointlightComponentData)
+		hmmwvChassisComponentData.components:add(LensFlare1)
+		hmmwvChassisComponentData.components:add(LensFlare2)
 
-		
+
+
 		print("Humweed goes poof")
 	else
 		print("A")
 	end
 
-	-- Venom 
-	local venomWeaponComponentData = nil
-	if ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("97945D87-011D-11E0-B97C-FC495C335A52"), Guid("39663C49-6C7F-429B-A00F-4885D0BBEDB8"))) then 
+	--Tank
+	local TankChassisComponentData = nil
+	--if ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("611F48A3-0919-11E0-985D-C512734E48AF"), Guid("DA89EB6C-A824-A813-A6C5-E2DEF9D4F4FF"))) then
+		--hmmwvChassisComponentData = ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("611F48A3-0919-11E0-985D-C512734E48AF"), Guid("DA89EB6C-A824-A813-A6C5-E2DEF9D4F4FF")))
+	--elseif ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("F7C250D2-ECEB-481F-A130-D91FE8B693E0"), Guid("5D10A701-196F-4DA9-8D83-610ABF56124B"))) then
+	TankChassisComponentData = ChassisComponentData(ResourceManager:FindInstanceByGuid(Guid("B069BA89-EECF-11DD-8117-9421284A74E5"), Guid("B06A08AD-EECF-11DD-8117-9421284A74E5")))
+	--else
+	--	print("No HMMVWV found")
+	--	return
+	--end
+
+	if TankChassisComponentData ~= nil then
+		-- Spotlights
+		local vehicleSpotLight = SpotLightEntityData()
+		vehicleSpotLight.shape = 1
+		vehicleSpotLight.coneInnerAngle = 10
+		vehicleSpotLight.coneOuterAngle = 5
+		vehicleSpotLight.frustumFov = 80
+		vehicleSpotLight.frustumAspect = 1.25
+		vehicleSpotLight.texture = TextureAsset(ResourceManager:FindInstanceByGuid(Guid("04C62561-2236-11DF-A528-EA655525F02D"), Guid("2EE018E8-1451-908C-0974-DB7676407D61")))
+		vehicleSpotLight.castShadowsMinLevel = 0
+		vehicleSpotLight.castShadowsEnable = true
+		vehicleSpotLight.intensity = 0.20
+		vehicleSpotLight.radius = 150
+
+		local vehicleLightComponentData = LightComponentData()
+		vehicleLightComponentData.light = vehicleSpotLight
+
+		vehicleLightComponentData.transform = LinearTransform(
+			Vec3(-1, 0, 0), --rotation
+			Vec3(0, 1, 0),
+			Vec3(0, 0, 1),
+			Vec3(-0.525, 0.65, 5) --pos (sideways,height,frontback)
+		)
+
+		local vehicleLightComponentData2 = LightComponentData()
+		vehicleLightComponentData2.light = vehicleSpotLight
+
+		vehicleLightComponentData2.transform = LinearTransform(
+			Vec3(-1, 0, 0), --rotation
+			Vec3(0, 1, 0),
+			Vec3(0, 0, 1),
+			Vec3(0.525, 0.65, 5) --pos (sideways,height,frontback)
+		)
+
+		-- PointLights
+		local VehiclePointlight = PointLightEntityData()
+		VehiclePointlight.width = 0.1
+
+		local VehiclePointlightComponentData = LightComponentData()
+		VehiclePointlightComponentData.light = VehiclePointlight
+
+		VehiclePointlightComponentData.transform = LinearTransform(
+			Vec3(-1, 0, 0), --rotation
+			Vec3(0, 1, 0),
+			Vec3(0, 0, 1),
+			Vec3(0.525, 1, 6) --pos (sideways,height,frontback)
+		)
+
+		local VehiclePointlightComponentData2 = LightComponentData()
+		VehiclePointlightComponentData2.light = VehiclePointlight
+
+		VehiclePointlightComponentData2.transform = LinearTransform(
+			Vec3(-1, 0, 0), --rotation
+			Vec3(0, 1, 0),
+			Vec3(0, 0, 1),
+			Vec3(-0.525, 1, 6) --pos (sideways,height,frontback)
+		)
+
+		-- Lens Flares
+		local LensFlare = LensFlareEntityData(ResourceManager:FindInstanceByGuid(Guid("65A5BFD9-028A-4D4F-8B89-3A60B2E06F83"), Guid("D8DB98E1-AEBA-485E-9AA4-D5F55C5CDECE")))
+		local LensFlare1 = LensFlareComponentData()
+		LensFlare1.lensFlare = LensFlare
+
+		LensFlare:MakeWritable()
+
+		for key, value in pairs(LensFlare.elements) do
+			value.sizeCamDistMax = value.sizeCamDistMax*2
+			value.sizeAngleCurve = value.sizeAngleCurve*1
+			value.size = value.size*0.34
+			value.sizeOccluderCurve = value.sizeOccluderCurve*1.5
+			value.sizeScreenPosCurve = value.sizeScreenPosCurve*1.5
+			value.alphaCamDistCurve = value.alphaCamDistCurve*1.5
+			value.alphaOccluderCurve = value.alphaOccluderCurve*1.5
+			value.alphaAngleCurve = value.alphaScreenPosCurve*1
+			value.alphaScreenPosCurve = value.alphaScreenPosCurve*1.5
+			value.alphaCamDistMax = value.alphaCamDistMax*2
+			print("LensFlare Changed")
+		end
+
+		LensFlare1.transform = LinearTransform(
+			Vec3(-1.0, 0.0, 8.74227765735e-08), --rotation
+			Vec3(0, 1, 0),
+			Vec3(-8.74227765735e-08, 0, -1),
+			Vec3(0.85, 1.3, 3.7) --pos (sideways,height,frontback)
+			)
+
+		local LensFlare2 = LensFlareComponentData()
+		LensFlare2.lensFlare = LensFlare
+
+		LensFlare2.transform = LinearTransform(
+			Vec3(-1.0, 0.0, 8.74227765735e-08), --rotation
+			Vec3(0, 1, 0),
+			Vec3(-8.74227765735e-08, 0, -1),
+			Vec3(-0.85, 1.3, 3.7) --pos (sideways,height,frontback)
+			)
+
+
+		-- Add new components to Vehicle Chassis
+
+		TankChassisComponentData:MakeWritable()
+
+		TankChassisComponentData.components:add(vehicleLightComponentData)
+		TankChassisComponentData.components:add(vehicleLightComponentData2)
+		TankChassisComponentData.components:add(LensFlare1)
+		TankChassisComponentData.components:add(LensFlare2)
+		TankChassisComponentData.components:add(VehiclePointlightComponentData)
+		TankChassisComponentData.components:add(VehiclePointlightComponentData2)
+
+		print("Tank goes poof")
+	else
+		print("A")
+	end
+
+
+	-- Venom
+	--[[local venomWeaponComponentData = nil
+	local venomWeaponComponentData2 = nil
+	if WeaponComponentData(ResourceManager:FindInstanceByGuid(Guid("97945D87-011D-11E0-B97C-FC495C335A52"), Guid("39663C49-6C7F-429B-A00F-4885D0BBEDB8"))) then
 		venomWeaponComponentData = WeaponComponentData(ResourceManager:FindInstanceByGuid(Guid("97945D87-011D-11E0-B97C-FC495C335A52"), Guid("39663C49-6C7F-429B-A00F-4885D0BBEDB8")))
-	else 
+		venomWeaponComponentData2 = WeaponComponentData(ResourceManager:FindInstanceByGuid(Guid("97945D87-011D-11E0-B97C-FC495C335A52"), Guid("C0F6F51D-0F12-41DE-8003-6252C78BE9E2")))
+		print("Found Venom")
+	else
 		print("No Venom found")
 		return
 	end
+
+
 
 	if venomWeaponComponentData ~= nil then
 		-- Spotlights
 		local weaponSpotlight = SpotLightEntityData()
 		weaponSpotlight.shape = 1
-		weaponSpotlight.coneInnerAngle = 10
-		weaponSpotlight.coneOuterAngle = 5
-		weaponSpotlight.frustumFov = 100
-		weaponSpotlight.frustumAspect = 1.25
+		weaponSpotlight.frustumFov = 35.6789
+		weaponSpotlight.frustumAspect = 1.0
 		weaponSpotlight.texture = TextureAsset(ResourceManager:FindInstanceByGuid(Guid("04C62561-2236-11DF-A528-EA655525F02D"), Guid("2EE018E8-1451-908C-0974-DB7676407D61")))
 		weaponSpotlight.castShadowsMinLevel = 0
 		weaponSpotlight.castShadowsEnable = true
-		weaponSpotlight.intensity = 0.20
-		weaponSpotlight.radius = 150
+		weaponSpotlight.intensity = 0.17
+		weaponSpotlight.radius = 1000
 
+		local weaponSpotlightComponentData = LightComponentData()
+		weaponSpotlightComponentData.light = weaponSpotlight
+
+		weaponSpotlightComponentData.transform = LinearTransform(
+			Vec3(-1, 0, 0), --rotation
+			Vec3(0, 1, 0),
+			Vec3(0, 0, 1),
+			Vec3(0, 0, 0.5) --pos (sideways,height,frontback)
+		)
+
+		local weaponSpotlightComponentData2 = LightComponentData()
+		weaponSpotlightComponentData2.light = weaponSpotlight
+
+		weaponSpotlightComponentData2.transform = LinearTransform(
+			Vec3(-1, 0, 0), --rotation
+			Vec3(0, 1, 0),
+			Vec3(0, 0, 1),
+			Vec3(0, 0, 0.5) --pos (sideways,height,frontback)
+		)
+
+		venomWeaponComponentData:MakeWritable()
+		venomWeaponComponentData.components:add(weaponSpotlightComponentData2)
+
+		venomWeaponComponentData2:MakeWritable()
+		venomWeaponComponentData2.components:add(weaponSpotlightComponentData2)
+
+
+		-- Lens Flares
+		local LensFlare = LensFlareEntityData(ResourceManager:FindInstanceByGuid(Guid("65A5BFD9-028A-4D4F-8B89-3A60B2E06F83"), Guid("D8DB98E1-AEBA-485E-9AA4-D5F55C5CDECE")))
+
+		LensFlare:MakeWritable()
+
+		for key, value in pairs(LensFlare.elements) do
+			value.sizeCamDistMax = value.sizeCamDistMax*4
+			value.sizeAngleCurve = value.sizeAngleCurve*0.5
+			value.size = value.size*0.35
+			value.sizeOccluderCurve = value.sizeOccluderCurve*1.5
+			value.sizeScreenPosCurve = value.sizeScreenPosCurve*1.5
+			value.alphaCamDistCurve = value.alphaCamDistCurve*1.5
+			value.alphaOccluderCurve = value.alphaOccluderCurve*1.5
+			value.alphaAngleCurve = value.alphaScreenPosCurve*0.75
+			value.alphaScreenPosCurve = value.alphaScreenPosCurve*1.5
+			value.alphaCamDistMax = value.alphaCamDistMax*4
+			print("LensFlare Changed")
+		end
+
+		local LensFlare1 = LensFlareComponentData()
+		LensFlare1.lensFlare = LensFlare
+
+		LensFlare1.transform = LinearTransform(
+			Vec3(-1.0, 0.0, 8.74227765735e-08), --rotation
+			Vec3(0, 1, 0),
+			Vec3(-8.74227765735e-08, 0, -1),
+			Vec3(0, -0.02, -0.001) --pos (sideways,height,frontback)
+			)
+
+		venomWeaponComponentData.components:add(LensFlare1)
+		venomWeaponComponentData2.components:add(LensFlare1)
+
+		print("Added Spotlight Venom")
+
+	end]]
+
+	-- AH1Z Viper
+	local viperWeaponComponentData = nil
+	if WeaponComponentData(ResourceManager:FindInstanceByGuid(Guid("643135EA-6CA7-11DF-B6FA-F715AA601362"), Guid("DF0AEBA7-A3D6-4CCA-9572-E6FBAB9569C5"))) then
+		viperWeaponComponentData = WeaponComponentData(ResourceManager:FindInstanceByGuid(Guid("643135EA-6CA7-11DF-B6FA-F715AA6013622"), Guid("DF0AEBA7-A3D6-4CCA-9572-E6FBAB9569C5")))
+		print("Found viper")
+	else
+		print("No viper found")
+		return
+	end
+
+
+
+	if viperWeaponComponentData ~= nil then
+
+		viperWeaponComponentData:MakeWritable()
+
+		-- Spotlights
+		local weaponSpotlight = SpotLightEntityData()
+		weaponSpotlight.shape = 1
+		weaponSpotlight.frustumFov = 35.6789
+		weaponSpotlight.frustumAspect = 1.0
+		weaponSpotlight.texture = TextureAsset(ResourceManager:FindInstanceByGuid(Guid("04C62561-2236-11DF-A528-EA655525F02D"), Guid("2EE018E8-1451-908C-0974-DB7676407D61")))
+		weaponSpotlight.castShadowsMinLevel = 0
+		weaponSpotlight.castShadowsEnable = true
+		weaponSpotlight.intensity = 0.28245
+		weaponSpotlight.radius = 1000
 		local weaponSpotlightComponentData = LightComponentData()
 		weaponSpotlightComponentData.light = weaponSpotlight
 
@@ -154,17 +400,58 @@ Events:Subscribe('Level:RegisterEntityResources', function(levelData)
 			Vec3(0, 0, 0) --pos (sideways,height,frontback)
 		)
 
-		venomWeaponComponentData:MakeWritable()
-		venomWeaponComponentData.components:add(weaponSpotlightComponentData)
+		viperWeaponComponentData.components:add(weaponSpotlightComponentData)
+		print("Added Spotlight viper")
 
+		local weaponPointlight = PointLightEntityData()
+		weaponPointlight.width = 0.35
+
+		local weaponPointlightComponentData = LightComponentData()
+		weaponPointlightComponentData.light = weaponPointlight
+
+		weaponPointlightComponentData.transform = LinearTransform(
+			Vec3(-1, 0, 0), --rotation
+			Vec3(0, 1, 0),
+			Vec3(0, 0, 1),
+			Vec3(0, 0, 0) --pos (sideways,height,frontback)
+		)
+
+		-- Lens Flares
+		local LensFlare = LensFlareEntityData(ResourceManager:FindInstanceByGuid(Guid("65A5BFD9-028A-4D4F-8B89-3A60B2E06F83"), Guid("D8DB98E1-AEBA-485E-9AA4-D5F55C5CDECE")))
+
+		LensFlare:MakeWritable()
+
+		for key, value in pairs(LensFlare.elements) do
+			value.sizeCamDistMax = value.sizeCamDistMax*4
+			value.sizeAngleCurve = value.sizeAngleCurve*0.75
+			value.size = value.size*0.35
+			value.sizeOccluderCurve = value.sizeOccluderCurve*1.5
+			value.sizeScreenPosCurve = value.sizeScreenPosCurve*1.5
+			value.alphaCamDistCurve = value.alphaCamDistCurve*1.5
+			value.alphaOccluderCurve = value.alphaOccluderCurve*1.5
+			value.alphaAngleCurve = value.alphaScreenPosCurve*0.75
+			value.alphaScreenPosCurve = value.alphaScreenPosCurve*1.5
+			value.alphaCamDistMax = value.alphaCamDistMax*4
+			print("LensFlare Changed")
+		end
+
+		local LensFlare1 = LensFlareComponentData()
+		LensFlare1.lensFlare = LensFlare
+
+		LensFlare1.transform = LinearTransform(
+			Vec3(-1.0, 0.0, 8.74227765735e-08), --rotation
+			Vec3(0, 1, 0),
+			Vec3(-8.74227765735e-08, 0, -1),
+			Vec3(0, -0.19, -1.1) --pos (sideways,height,frontback)
+			)
+
+		viperWeaponComponentData.components:add(weaponPointlightComponentData)
+		viperWeaponComponentData.components:add(LensFlare1)
+		print("Added Pointlight viper")
 	end
 
 
 end)
-	
-
-
-
 
 
 
@@ -200,24 +487,24 @@ end)
 	local VehicleModifications = class 'VehicleModifications'
 
 	local Spotlights = {}
-	local Spotlights.componentdata = {}
+	Spotlights.componentdata = {}
 	local Vehicles = {}
 
 
 	function VehicleModifications:Scan(vehicle, partitionGUID, instanceGUID, chassisGUID)
-		if VehicleEntityData(ResourceManager:FindInstanceByGuid(partitionGUID, instanceGUID) then 
-			Vehicles.vehicle = ChassisComponentData(ResourceManager:FindInstanceByGuid(ResourceManager:FindInstanceByGuid(partitionGUID, chassisGUID))
-		else 
-			print("No HMMVWV found")
+		if VehicleEntityData(ResourceManager:FindInstanceByGuid(partitionGUID, instanceGUID)) then
+			Vehicles.vehicle = ChassisComponentData(ResourceManager:FindInstanceByGuid(ResourceManager:FindInstanceByGuid(partitionGUID, chassisGUID)))
+		else
+			print("No" ..vehicle.. " found")
 			return
 		end
 	end
 
 	function VehicleModifications:ScanWeapons(partitionGUID, instanceGUID, weaponGUID)
-		if VehicleEntityData(ResourceManager:FindInstanceByGuid(partitionGUID, instanceGUID) then 
-			Vehicles.vehicle = ChassisComponentData(ResourceManager:FindInstanceByGuid(ResourceManager:FindInstanceByGuid(partitionGUID, chassisGUID))
-		else 
-			print("No HMMVWV found")
+		if VehicleEntityData(ResourceManager:FindInstanceByGuid(partitionGUID, instanceGUID)) then
+			Vehicles.vehicle = ChassisComponentData(ResourceManager:FindInstanceByGuid(ResourceManager:FindInstanceByGuid(partitionGUID, chassisGUID)))
+		else
+			print("No" ..vehicle.. " found")
 			return
 		end
 	end
@@ -255,17 +542,16 @@ end)
         	local newInstance = _G[instanceType]()
 			newInstance:MakeWritable()
 
-			for key, value in pairs(Spotlights.componentdata) do 
+			for key, value in pairs(Spotlights.componentdata) do
 				key.components:add(value)
 			end
-			
+
 		end
 	end
 
 	function VehicleModifications:AddComponentsToVehicleWeapon()
 		hmmwvChassisComponentData:MakeWritable()
-		for key, value in pairs(Spotlights.componentdata) do 
+		for key, value in pairs(Spotlights.componentdata) do
 			hmmwvChassisComponentData.components:add(key)
 		end
 	end
-
