4dea46626858895a1a6966ac16c0d2e4cde96a36
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2021-05-24 21:11:57 +0200

    Code Refactor Commit 5 Shared
    - new datatables folder
    - vehiclemodifications lua

---
+++ refactored code/shared/DebugGUI.lua
@@ -0,0 +1,299 @@
+---
+--- Author:   Nikos Kapraras <nikos@kapraran.dev>
+--- URL:      https://github.com/kapraran/vu-debug-gui
+--- License:  https://choosealicense.com/licenses/mit/
+---
+
+DebugGUIControlType = {
+  Button = 1,
+  Checkbox = 2,
+  Text = 3,
+  Range = 4,
+  Dropdown = 5,
+  Number = 6
+}
+
+--
+-- DebugGUIControl
+--
+
+class "DebugGUIControl"
+
+DebugGUIControl.static.OrderIndex = 1
+
+function DebugGUIControl:__init(_type, name, options, context, callback)
+  if callback == nil then
+    callback = context
+    context = nil
+  end
+
+  self.id = MathUtils:RandomGuid()
+  self.type = _type
+  self.name = name
+  self.options = options
+  self.context = context
+  self.callback = callback
+  self.isClient = SharedUtils:IsClientModule()
+
+  self.lastValue = options.DefValue
+  self.folder = nil
+  self.order = DebugGUIControl.OrderIndex
+
+  DebugGUIControl.static.OrderIndex = DebugGUIControl.OrderIndex + 1
+end
+
+function DebugGUIControl:ExecuteCallback(value, player)
+  self.lastValue = value
+
+  if self.callback == nil then
+    return
+  end
+
+  if self.context == nil then
+    self.callback(value, player)
+  else
+    self.callback(self.context, value, player)
+  end
+end
+
+function DebugGUIControl:Get()
+  return self.lastValue
+end
+
+function DebugGUIControl:AsTable()
+  return {
+    Id = self.id:ToString("D"),
+    Type = self.type,
+    Name = self.name,
+    Folder = self.folder,
+    Options = self.options,
+    IsClient = self.isClient,
+  }
+end
+
+--
+-- DebugGUIManager
+--
+
+class "DebugGUIManager"
+
+function DebugGUIManager:__init()
+  self.controls = {}
+
+  self.__controlsRequested = false
+  self.__addInFolder = nil
+
+  self:RegisterEvents()
+end
+
+function DebugGUIManager:RegisterEvents()
+  Events:Subscribe("DBGUI:RequestControls", self, self.OnRequestControls)
+  NetEvents:Subscribe("DBGUI:RequestControls.Net", self, self.OnRequestControls)
+
+  if SharedUtils:IsClientModule() then
+    Events:Subscribe("DBGUI:OnChange", self, self.OnChange)
+  else
+    NetEvents:Subscribe("DBGUI:OnChange.Net", self, self.OnChangeNet)
+  end
+end
+
+function DebugGUIManager:OnChange(id, value, player)
+  local control = self.controls[id]
+
+  if control == nil then
+    return
+  end
+
+  control:ExecuteCallback(value, player)
+end
+
+function DebugGUIManager:OnChangeNet(player, id, value)
+  self:OnChange(id, value, player)
+end
+
+function DebugGUIManager:Add(control)
+  if control == nil then
+    return nil
+  end
+
+  if self.__addInFolder ~= nil then
+    control.folder = self.__addInFolder
+  end
+
+  self.controls[control.id:ToString("D")] = control
+
+  if self.__controlsRequested then
+    self.__controlsRequested = false
+    self:Show(false)
+  end
+
+  return control
+end
+
+function DebugGUIManager:Folder(name, context, callback)
+  -- avoid nested folders
+  if self.__addInFolder ~= nil then
+    return
+  end
+
+  -- swap callback-context if needed
+  if callback == nil then
+    if context == nil then
+      return
+    end
+
+    callback = context
+    context = nil
+  end
+
+  self.__addInFolder = name
+  callback(context)
+  self.__addInFolder = nil
+end
+
+function DebugGUIManager:OnRequestControls()
+  self.__controlsRequested = true
+  self:Show(false)
+end
+
+function DebugGUIManager:Show(clear)
+  if not self.__controlsRequested then
+    return
+  end
+
+  clear = not (not clear)
+
+  -- convert to array
+  local controlsOrdered = {}
+  for _, control in pairs(self.controls) do
+    table.insert(controlsOrdered, control)
+  end
+
+  -- sort based on .order
+  table.sort(controlsOrdered, function(controlA, controlB)
+    return controlA.order < controlB.order
+  end)
+
+  local data = {}
+  for _, control in ipairs(controlsOrdered) do
+    table.insert(data, control:AsTable())
+  end
+
+  if SharedUtils:IsClientModule() then
+    Events:Dispatch("DBGUI:Show", clear, data)
+  else
+    NetEvents:Broadcast("DBGUI:Show.Net", clear, data)
+  end
+end
+
+local debugGUIManager = DebugGUIManager()
+
+--
+-- DebugGUI
+--
+
+class "DebugGUI"
+
+function DebugGUI.static:Button(name, context, callback)
+  local control = DebugGUIControl(
+    DebugGUIControlType.Button,
+    name,
+    {},
+    context,
+    callback
+  )
+
+  return debugGUIManager:Add(control)
+end
+
+function DebugGUI.static:Checkbox(name, defValue, context, callback)
+  local control = DebugGUIControl(
+    DebugGUIControlType.Checkbox,
+    name,
+    {
+      DefValue = defValue
+    },
+    context,
+    callback
+  )
+
+  return debugGUIManager:Add(control)
+end
+
+function DebugGUI.static:Text(name, defValue, context, callback)
+  local control = DebugGUIControl(
+    DebugGUIControlType.Text,
+    name,
+    {
+      DefValue = defValue
+    },
+    context,
+    callback
+  )
+
+  return debugGUIManager:Add(control)
+end
+
+function DebugGUI.static:Number(name, defValue, context, callback)
+  local control = DebugGUIControl(
+    DebugGUIControlType.Number,
+    name,
+    {
+      DefValue = defValue
+    },
+    context,
+    callback
+  )
+
+  return debugGUIManager:Add(control)
+end
+
+function DebugGUI.static:Range(name, options, context, callback)
+  options = options or {}
+
+  -- defaults
+  options.Min = (options.Min ~= nil and options.Min) or 0
+  options.Max = (options.Max ~= nil and options.Max) or 100
+  options.Step = (options.Step ~= nil and options.Step) or 1
+  options.DefValue = options.DefValue or options.Min
+
+  local control = DebugGUIControl(
+    DebugGUIControlType.Range,
+    name,
+    options,
+    context,
+    callback
+  )
+
+  return debugGUIManager:Add(control)
+end
+
+function DebugGUI.static:Dropdown(name, options, context, callback)
+  options = options or {}
+
+  -- defaults
+  options.Values = (options.Values ~= nil and options.Values) or {0}
+  options.DefValue = options.DefValue or 0
+
+  local control = DebugGUIControl(
+    DebugGUIControlType.Dropdown,
+    name,
+    options,
+    context,
+    callback
+  )
+
+  return debugGUIManager:Add(control)
+end
+
+function DebugGUI.static:Print(str)
+  -- TODO
+end
+
+function DebugGUI.static:Folder(name, context, callback)
+  debugGUIManager:Folder(name, context, callback)
+end
+
+function DebugGUI.static:Show(clear)
+  debugGUIManager:Show(clear)
+end
---
+++ refactored code/shared/vehiclemodifications.lua
@@ -0,0 +1,286 @@
+local Vehicle = class('Vehicle')
+local Settings = require '__shared/settings'
+local RM = require '__shared/classes/tools/resourcemanager'
+local Tool = require '__shared/classes/tools/tool'
+local VehicleSettings = require '__shared/vehicles'
+local vehicleEntityData = nil 
+
+
+function Vehicle:__Init()
+
+    Vehicle:RegisterEvents()
+
+end 
+
+
+function Vehicle:RegisterEvents()
+
+    self.entityResourcesRegisterEvent = Events:Subscribe('Level:RegisterEntityResources', self, self.OnEntityRegister)
+
+end 
+
+
+function Vehicle:OnEntityRegister()
+
+    for typeIndex, vehicleType in pairs(VehicleSettings) do
+
+        for vehicleIndex, vehicle in pairs(vehicleType) do
+
+           Tool:DebugPrint(vehicle.name .." | ".. vehicle.partitionGUID .." |  ".. vehicle.chassisGUID, 'adding')
+           chassisData = RM:Find(vehicle.partitionGUID, vehicle.chassisGUID)				
+
+               if chassisData ~= nil then
+                   chassisData = ChassisComponentData(chassisData)
+
+                    for componentIndex, component in pairs(vehicle.Components) do
+
+                        if component.type == 1 and component.enabled then
+
+                           AddPointLight(component, chassisData, vehicle)
+
+                        elseif component.type == 2 and component.enabled then
+
+                           AddSpotLight(component, chassisData, vehicle)
+
+                       end
+
+                    end
+               Tool:DebugPrint('--------------------------------------------------', 'adding')
+               end
+               
+        end
+        
+
+    end
+
+end 
+
+
+function Vehicle:AddPointLight(pointLightSettingsArray, chassisData, vehicle)
+
+	if vehicle == nil then
+		return
+	end
+
+	vehicleEntityData = RM:VED(vehicle.partitionGUID, vehicle.vehicleDataGUID) 				
+	vehicleEntityData:MakeWritable()
+
+ 	local newPointLight = PointLightEntityData()
+ 	newPointLight.radius = pointLightSettingsArray.radius
+ 	--newPointLight.width = pointLightSettingsArray.width
+ 	newPointLight.color = pointLightSettingsArray.color
+ 	newPointLight.intensity = pointLightSettingsArray.intensity
+ 	newPointLight.visible = pointLightSettingsArray.visible
+
+ 	local newPointlightComponentData = LightComponentData()
+ 	newPointlightComponentData.light = newPointLight
+ 	newPointlightComponentData.transform = LinearTransform(
+ 		pointLightSettingsArray.transform.left,
+ 		pointLightSettingsArray.transform.up,
+ 		pointLightSettingsArray.transform.forward,
+ 		pointLightSettingsArray.transform.trans
+ 	)
+
+ 	if pointLightSettingsArray.weaponmounted then
+
+		local weaponData = nil
+
+		if pointLightSettingsArray.weapon2 == true then
+
+			weaponData = RM:WCP(vehicle.partitionGUID, vehicle.weaponGUID2)	
+            
+	   	else
+
+		   	weaponData = RM:WCP(vehicle.partitionGUID, vehicle.weaponGUID)	
+
+	   	end
+
+ 		if weaponData.isReadOnly then
+
+ 			weaponData:MakeWritable()
+
+ 		end
+		
+ 		weaponData.components:add(newPointlightComponentData)
+        vehicleEntityData.runtimeComponentCount = vehicleEntityData.runtimeComponentCount + 1
+
+		Tool:DebugPrint('Added weapon pointlight to ' .. vehicle.name, 'adding')
+		
+ 	else
+
+ 		if chassisData.isReadOnly then
+
+ 			chassisData:MakeWritable()
+
+ 		end
+
+ 		chassisData.components:add(newPointlightComponentData)
+        vehicleEntityData.runtimeComponentCount = vehicleEntityData.runtimeComponentCount + 1
+		Tool:DebugPrint('Added '..pointLightSettingsArray.description..' to ' .. vehicle.name, 'adding')
+
+ 	end
+
+end
+
+
+function Vehicle:AddSpotLight(spotLightSettingsArray, chassisData, vehicle)
+
+	if vehicle == nil then
+	    return
+	end
+
+	vehicleEntityData = RM:VED(vehicle.partitionGUID, vehicle.vehicleDataGUID)				
+	vehicleEntityData:MakeWritable()
+
+ 	local newSpotLight = SpotLightEntityData()
+ 	newSpotLight.shape = spotLightSettingsArray.shape
+ 	newSpotLight.intensity = spotLightSettingsArray.intensity
+ 	newSpotLight.color = spotLightSettingsArray.color
+ 	newSpotLight.frustumFov = spotLightSettingsArray.frustumFov
+ 	newSpotLight.frustumAspect = spotLightSettingsArray.frustumAspect
+ 	newSpotLight.texture = RM:Flashlight()													
+ 	newSpotLight.castShadowsMinLevel = 0
+ 	newSpotLight.castShadowsEnable = false
+ 	newSpotLight.radius = spotLightSettingsArray.radius
+
+ 	local newSpotlightComponentData = LightComponentData()
+ 	newSpotlightComponentData.light = newSpotLight
+ 	newSpotlightComponentData.transform = LinearTransform(
+ 		spotLightSettingsArray.transform.left,
+ 		spotLightSettingsArray.transform.up,
+ 		spotLightSettingsArray.transform.forward,
+ 		spotLightSettingsArray.transform.trans
+ 	)
+
+ 	if spotLightSettingsArray.weaponmounted then
+
+		local weaponData = nil
+
+		if spotLightSettingsArray.weapon2 == true then
+
+ 			weaponData = RM:WCP(vehicle.partitionGUID, vehicle.weaponGUID2) 	
+
+		else
+
+			weaponData = RM:WCP(vehicle.partitionGUID, vehicle.weaponGUID)		
+
+		end
+
+ 		if weaponData.isReadOnly then
+
+ 			weaponData:MakeWritable()
+
+ 		end
+
+		Vehicle:AddLensFlare(spotLightSettingsArray.name, weaponData, spotLightSettingsArray.transform.trans, vehicle) 
+		 
+ 		weaponData.components:add(newSpotlightComponentData)
+		vehicleEntityData.runtimeComponentCount = vehicleEntityData.runtimeComponentCount + 1
+
+		Tool:DebugPrint('Added weapon spotlight to ' .. vehicle.name, 'adding')
+
+ 	else
+
+ 		if chassisData.isReadOnly then
+
+ 			chassisData:MakeWritable()
+
+ 		end
+
+		Vehicle:AddLensFlare(spotLightSettingsArray.name, chassisData, spotLightSettingsArray.transform.trans, vehicle)
+
+ 		chassisData.components:add(newSpotlightComponentData)
+		vehicleEntityData.runtimeComponentCount = vehicleEntityData.runtimeComponentCount + 1
+
+		Tool:DebugPrint('Added '..spotLightSettingsArray.description..' to ' .. vehicle.name, 'adding')
+
+		if spotLightSettingsArray.transform.mirrored == true then
+
+		Vehicle:AddMirrorSpotlight(spotLightSettingsArray.name, chassisData, spotLightSettingsArray, vehicle)
+
+		end 
+
+ 	end
+
+end
+
+
+function Vehicle:AddLensFlare(name, data, trans, vehicle)
+
+	if vehicle == nil then
+		return
+	end
+
+	if vehicle.name == 'SU35BM' or vehicle.name == 'F35' then
+		return
+	end
+
+	local LensFlare = RM:LFED("65A5BFD9-028A-4D4F-8B89-3A60B2E06F83","D8DB98E1-AEBA-485E-9AA4-D5F55C5CDECE")
+	LensFlare:MakeWritable()
+
+	for key, value in pairs(LensFlare.elements) do
+
+		local dist = 80
+		local dist2 = 60
+		local sizeAngle = Vec4(0.13, 1, 0, 0)
+		local alphaAngle = Vec4(0.04, 1, 0, 0)
+
+		value.sizeCamDistMax = dist
+		value.alphaCamDistMax = dist2
+		value.sizeAngleCurve = sizeAngle
+		value.alphaAngleCurve = alphaAngle
+		value.sizeScreenPosCurve = value.sizeScreenPosCurve
+		value.alphaScreenPosCurve = value.alphaScreenPosCurve
+		value.size = value.size * 0.92
+		value.sizeOccluderCurve = value.sizeOccluderCurve * 1
+		value.alphaCamDistCurve = value.alphaCamDistCurve * 1
+		value.alphaOccluderCurve = value.alphaOccluderCurve * 1
+
+	end
+
+	local name = LensFlareComponentData()
+	name.lensFlare = LensFlare
+
+	name.transform = LinearTransform(
+		Vec3(-1.0, 0.0, 8.74227765735e-08), --rotation
+		Vec3(0, 1, 0),
+		Vec3(-8.74227765735e-08, 0, -1),
+		trans
+		)
+
+	data.components:add(name)
+	vehicleEntityData.runtimeComponentCount = vehicleEntityData.runtimeComponentCount + 1
+
+end
+
+
+function Vehicle:AddMirrorSpotlight(name, data, settings, vehicle)
+
+	local newSpotLight = SpotLightEntityData()
+ 	newSpotLight.shape = settings.shape
+ 	newSpotLight.intensity = settings.intensity
+ 	newSpotLight.color = settings.color
+ 	newSpotLight.frustumFov = settings.frustumFov
+ 	newSpotLight.frustumAspect = settings.frustumAspect
+ 	newSpotLight.texture = RM:Flashlight()
+ 	newSpotLight.castShadowsMinLevel = 0
+ 	newSpotLight.castShadowsEnable = false
+ 	newSpotLight.radius = settings.radius
+
+ 	local name = LightComponentData()
+ 	name.light = newSpotLight
+ 	name.transform = LinearTransform(
+		settings.transform.left,
+		settings.transform.up,
+		settings.transform.forward,
+		settings.transform.trans * Vec3((-1), 1, 1)
+ 	)
+
+	data.components:add(name)
+	vehicleEntityData.runtimeComponentCount = vehicleEntityData.runtimeComponentCount + 1
+
+	Vehicle:AddLensFlare(name, data, settings.transform.trans * Vec3((-1), 1, 1), vehicle)
+
+	Tool:DebugPrint('Added ' .. settings.description .. ' Mirror Spotlight', 'adding')
+
+end
