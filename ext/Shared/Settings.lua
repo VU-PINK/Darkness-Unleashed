acf4f268305360fd03652b3614098c61990fb451
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2021-11-01 06:19:42 +0100

    giiiit

---
+++ ext/Shared/Settings.lua
@@ -0,0 +1,45 @@
+-- Default MapVE Configuration
+CONFIG = {
+    MAPS = {
+        MP_001 = 'Night', -- Grand Bazaar
+        MP_003 = 'Night', -- Teheran Highway
+        MP_007 = 'Night', -- Caspian Border
+        MP_011 = 'Night', -- Seine Crossing
+        MP_012 = 'Night', -- Operation Firestorm
+        MP_013 = 'Night', -- Damavand Peak
+        MP_017 = 'Night', -- Noshahr Canals
+        MP_018 = 'Night', -- Kharg Island
+        MP_Subway = 'Night', -- Operation Metro
+        XP1_001 = 'Night', -- Strike at Karkand
+        XP1_002 = 'Night', -- Gulf of Oman
+        XP1_003 = 'Night', -- Sharqi Peninsula
+        XP1_004 = 'Night', -- Wake Island
+        XP2_Palace = 'Night', -- Donya Fortress
+        XP2_Office = 'Night', -- Operation 925
+        XP2_Factory = 'Night', -- Scrapmetal
+        XP2_Skybar = 'Night', -- Ziba Tower
+        XP3_Alborz = 'Night', -- Alborz Mountains
+        XP3_Shield = 'Night', -- Armored Shield
+        XP3_Desert = 'Night', -- Bandar Desert
+        XP3_Valley = 'Night', -- Death Valley
+        XP4_Parl = 'Night', -- Azadi Palace
+        XP4_Quake = 'Night', -- Epicenter
+        XP4_FD = 'Night', -- Markaz Monolith
+        XP4_Rubble = 'Night', -- Talah Market
+        XP5_001 = 'Night', -- Operation Riverside
+        XP5_002 = 'Night', -- Nebandan Flats
+        XP5_003 = 'Night', -- Kiasar Railroad
+        XP5_004 = 'Night' -- Sabalan Pipeline
+    },
+    VEHICLES = {
+        USE_VEHICLE_LIGHTS_AIRBORNE = false,
+        USE_VEHICLE_LIGHTS_GROUND = false
+    },
+    GENERAL = {
+        USE_NIGHTVISION_GADGET = true
+    },
+    LOGGER_ENABLED = true,
+    LOGGER_PRINT_ALL = true
+}
+
+return CONFIG
---
+++ ext/Shared/__init__.lua
@@ -0,0 +1,4 @@
+require("__shared/Settings")
+require("__shared/VehicleSettings")
+require("__shared/Tools/Logger")
+require("__shared/Tools/RM")
