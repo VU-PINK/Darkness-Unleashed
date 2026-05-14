69abb0de9d668f65022f27ed56e271e76a731582
Augustakit <102933021+Augustakit@users.noreply.github.com>
2026-05-14 19:31:02 +0800

    Delete ext directory

--- ext/Shared/__init__.lua
+++
@@ -1,6 +0,0 @@
-require("__shared/Settings")
-require("__shared/VehicleSettings")
-require("__shared/Tools/Logger")
-require("__shared/Tools/RM")
-require("__shared/Tools/Functions")
--- require("__shared/VehicleManager")
